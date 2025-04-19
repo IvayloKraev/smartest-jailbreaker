4b5c8d2a004b3a020b390b1a53c706aef02066ba
IvayloKraev <ivaylokraev@protonmail.com>
2025-04-20 00:15:41 +0300

    Make substitute for fetch

--- src/substitute.ts
+++ src/substitute.ts
@@ -1,3 +1,31 @@
-console.log = function (...data: any[]) {
-    console.error(data)
-}
+const baseFetch = window.fetch
+
+window.fetch = async function (input: RequestInfo | URL, init?: RequestInit): Promise<Response> {
+    if (
+        typeof input === "string" &&
+        input.startsWith("https://api.smartest.bg/api/testSessions/") &&
+        input.endsWith("/details")
+    ) {
+        const response = await baseFetch(input, init)
+        const data = await response.json()
+
+        const modifiedData = {
+            ...data,
+            strictMode: false
+        }
+
+        const modifiedBlob = new Blob([JSON.stringify(modifiedData)], { type: 'application/json' });
+
+        return new Response(
+            modifiedBlob,
+            {
+                headers: response.headers,
+                status: response.status,
+                statusText: response.statusText,
+            }
+        )
+    }
+    else {
+        return baseFetch(input, init)
+    }
+};
