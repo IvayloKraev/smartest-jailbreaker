d770ebd29b4226f0b5e6e5222698a6bec7bc0ad9
IvayloKraev <ivaylokraev@protonmail.com>
2025-04-20 00:06:24 +0300

    Import script on loading

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,6 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (tab.url && tab.url.startsWith("https://www.smartest.bg/")) {
-        if (changeInfo.status === "complete") {
+        if (changeInfo.status === "loading") {
             chrome.scripting.executeScript({
                 target: { tabId },
                 world: "MAIN",
